aff2c3f8d7756c6ab4fe282129dccd59e06d0139
MondaleFelix <mondalefelix@yahoo.com>
2019-12-04 11:20:53 -0800

    labels update when buttons pressed

--- BrainGames/BrainGames/ViewController.swift
+++ BrainGames/BrainGames/ViewController.swift
@@ -11,16 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var color2: UILabel!
+    @IBOutlet weak var color1: UILabel!
     var colors = ["red", "blue", "green", "yellow"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateColors()
+    
         // Do any additional setup after loading the view.
     }
 
 
     @IBAction func answerPressed(_ sender: UIButton) {
         print(sender.currentTitle!)
+        
+        updateColors()
+    }
+    
+    func updateColors(){
+        color1.text = colors.randomElement()
+        color2.text = colors.randomElement()
+
     }
 }
 
