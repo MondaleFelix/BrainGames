33eebadd965ce66545598062b9e10d6b58ebcf33
MondaleFelix <mondalefelix@yahoo.com>
2019-12-05 11:24:29 -0800

    label changes colors when button pressed

--- BrainGames/BrainGames/ViewController.swift
+++ BrainGames/BrainGames/ViewController.swift
@@ -13,7 +13,43 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var color2: UILabel!
     @IBOutlet weak var color1: UILabel!
-    var colors = ["red", "blue", "green", "yellow"]
+    
+    enum Color {
+        case red
+        case blue
+        case green
+        case yellow
+        
+        var colorName: String{
+            switch self {
+            case .red:
+                return "red"
+            case .blue:
+                return "blue"
+            case .green:
+                return "green"
+            case .yellow:
+                return "yellow"
+            }
+        }
+        
+        var colorIdentifier: UIColor {
+            switch self {
+            case .red:
+                return .red
+            case .blue:
+                return .blue
+            case .green:
+                return .green
+            case .yellow:
+                return .yellow
+            }
+        }
+    }
+    
+    var colors = [Color.red, Color.blue, Color.green, Color.yellow ]
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +66,9 @@ class ViewController: UIViewController {
     }
     
     func updateColors(){
-        color1.text = colors.randomElement()
-        color2.text = colors.randomElement()
+        color1.text = colors.randomElement()?.colorName
+        color2.text = colors.randomElement()?.colorName
+        color2.textColor = colors.randomElement()?.colorIdentifier
 
     }
 }
